d9f1f6653c1608ac914fdd359d0d2aa3e92d65b5
hammad <hbhatti@adee.com.au>
2025-09-08 13:59:54 +1000

    azapi_resource" "job_target_group"

--- CPA-Mobile-App/rg-shared.tf
+++ CPA-Mobile-App/rg-shared.tf
@@ -831,23 +831,23 @@ resource "azurerm_mssql_job_target_group" "job_target_group_server" {
 # Azurerm - Manages a Job Target Group using AzAPI
 # ------------------------------------------------------------
 resource "azapi_resource" "job_target_group" {
-  provider = azapi
   type      = "Microsoft.Sql/servers/jobAgents/targetGroups@2021-11-01-preview"
+  provider = azapi
   name      = local.sql_job_target_group_name_jobdb
   parent_id = azurerm_mssql_job_agent.sql_job_agent.id
 
-  body = jsonencode({
+  body = {
     properties = {
       members = [
         {
           type           = "SqlServer"
           serverName     = module.avm-res-sql-server-shared.resource_name
           membershipType = "Include"
-          # Notice: no jobCredentialId here
+          # No jobCredentialId here
         }
       ]
     }
-  })
+  }
 }
 
 # # ------------------------------------------------------------
